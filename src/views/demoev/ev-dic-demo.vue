602832928f369b9598ac68a0048b75af128779d7
18288941965 <2405077320@qq.com>
2023-12-05 15:18:39 +0800

    规范化命名了css样式

--- src/views/demoev/ev-dic-demo.vue
+++ src/views/demoev/ev-dic-demo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <ev-select-demo style="box-shadow: var(--color-shadow-small); border: var(--color-border-1); padding: 20px;border-radius: var(--border-radius-medium);margin-bottom: 20px;"></ev-select-demo>
-    <ev-checkbox-demo style="box-shadow: var(--color-shadow-small); border: var(--color-border-1); padding: 20px;border-radius: var(--border-radius-medium);margin-bottom: 20px;"></ev-checkbox-demo>
-    <ev-radio-demo style="box-shadow: var(--color-shadow-small); border: var(--color-border-1); padding: 20px;border-radius: var(--border-radius-medium);margin-bottom: 20px;"></ev-radio-demo>
+    <ev-select-demo style="box-shadow: var(--box-shadow-small); border: var(--border-1); padding: 20px;border-radius: var(--border-radius-medium);margin-bottom: 20px;"></ev-select-demo>
+    <ev-checkbox-demo style="box-shadow: var(--box-shadow-small); border: var(--border-1); padding: 20px;border-radius: var(--border-radius-medium);margin-bottom: 20px;"></ev-checkbox-demo>
+    <ev-radio-demo style="box-shadow: var(--box-shadow-small); border: var(--border-1); padding: 20px;border-radius: var(--border-radius-medium);margin-bottom: 20px;"></ev-radio-demo>
   </div>
 </template>
 
