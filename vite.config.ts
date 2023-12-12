a0f6f680575460bcc367207ac2f476c954d6d990
18288941965 <2405077320@qq.com>
2023-12-12 14:47:53 +0800

    调整css的变量定义

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        loader: "sass-loader",
-        additionalData: `
-        $mg-ultra-small: 5px;
-$mg-small: 10px;
-$mg-medium: 15px;
-$mg-large: 20px;
-$mg-ultra-large: 30px;
-
-$pd-ultra-small: 5px;
-$pd-small: 10px;
-$pd-medium: 15px;
-$pd-large: 20px;
-$pd-ultra-large: 30px;
-
-$lh-small: 32px;
-$lh-medium: 40px;
-$lh-large: 60px;
-
-$panel-width-medium: 260px;
-        `,
-      },
-    },
-  },
+  plugins: [vue()]
 })
