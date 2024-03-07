01ec9c7f765ca7285a62d215f6666dcecdc81d7d
18288941965 <2405077320@qq.com>
2024-03-07 10:06:12 +0800

    修改主题3适配多层级菜单

--- src/views/admin/admin-theme3.vue
+++ src/views/admin/admin-theme3.vue
@@ -12,22 +12,54 @@
       v-if="menus && menus.length > 0"
       class="admin-theme__nav"
     >
-      <ul>
+      <ul class="menu-wrapper">
         <template
           v-for="(menu, index) in menus[0].children"
           :key="'li-3-' + index"
         >
-          <li
-            v-if="menu.url"
-          >
-            <router-link
-              class="nav-item"
-              :to="menu.url"
-              @click="pushRouter(menu)"
+          <template v-if="menu.children && menu.children.length > 0">
+            <li
+              id="theme-menu-group"
+              class="theme-menu-group"
+              :class="{'active-menu-group': activeMenuGroupId === menu.id}"
             >
-              <span>{{ menu.label }}</span>
-            </router-link>
-          </li>
+              <details
+                :id="'menu-detail-' + index"
+                class="menu-group-details arrow-down"
+              >
+                <summary>
+                  {{ menu.label }}
+                  <Expand
+                    :size="10"
+                    class="expand-mg"
+                  />
+                </summary>
+                <div
+                  class="menu-group-details-content menu-scroll"
+                  @click="closeDetails('menu-detail-' + index)"
+                >
+                  <admin-menu-top-child
+                    :menus="menu.children"
+                    :menu-id="activeMenus.menuId"
+                    @push-router="pushRouter"
+                  />
+                </div>
+              </details>
+            </li>
+          </template>
+          <template v-else>
+            <li
+              v-if="menu.url"
+            >
+              <router-link
+                class="nav-item"
+                :to="menu.url"
+                @click="pushRouter(menu)"
+              >
+                <span>{{ menu.label }}</span>
+              </router-link>
+            </li>
+          </template>
         </template>
       </ul>
     </nav>
@@ -44,18 +76,26 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted} from 'vue'
+import {defineComponent, ref, onMounted} from 'vue'
 import AdminMenu from './menu/admin-menu.vue'
 import {MenuBean} from '../../interface/menuInterface'
 import {MenuContext, MenuStatusContext} from '../../context/menuContext'
 import AdminHeader from './header/admin-header.vue'
 import {themeBaseContext, updateBrowserTitle} from './adminThemeBase'
+import AdminMenuTopChild from './menu/admin-menu-top-child.vue'
+import {
+  Expand
+} from '../../components/svicon/publicIcon'
+import {closeDetails} from '../../util/baseUtil'
+import menuDfs from '../../algo/menuDfs'
 
 export default defineComponent({
   name: 'AdminTheme3',
   components: {
+    Expand,
     AdminMenu,
-    AdminHeader
+    AdminHeader,
+    AdminMenuTopChild
   },
   setup () {
     const {
@@ -68,6 +108,9 @@ export default defineComponent({
       menus,
       getMenus
     } = MenuContext()
+
+    // 激活菜单的父菜单ID
+    const activeMenuGroupId = ref('')
 
     const {
       activeMenus,
@@ -82,6 +125,13 @@ export default defineComponent({
       }
       updateActiveMenus(menu)
       updateBrowserTitle(`${menu.label as string} • ${rootMenu.label}`)
+
+      const menuPath = menuDfs(menus.value[0], activeMenus.menuId, false)
+      if (menuPath.length === 0 || menuPath[0].id === activeMenus.menuId) {
+        activeMenuGroupId.value = ''
+      } else {
+        activeMenuGroupId.value = menuPath[0].id
+      }
     }
 
     onMounted(() => {
@@ -94,7 +144,9 @@ export default defineComponent({
       keepAliveInclude,
       pushRouter,
       menus,
-      activeMenus
+      activeMenus,
+      closeDetails,
+      activeMenuGroupId
     }
   }
 })
---
+++ src/views/admin/menu/admin-menu-top-child.vue
@@ -0,0 +1,82 @@
+<template>
+  <ul class="admin-menu-top-child">
+    <template
+      v-for="(menu, index) in menus"
+      :key="'menu-' + index"
+    >
+      <template v-if="menu.children && menu.children.length > 0">
+        <li
+          class="menu-label"
+          :style="{'--menu-level': menuLevel}"
+        >
+          {{ menu.label }}
+        </li>
+        <admin-menu-top-child
+          :menu-id="menuId"
+          :menus="menu.children"
+          :menu-level="menuLevel + 1"
+          @push-router="pushRouter"
+        />
+      </template>
+
+      <li
+        v-else
+        class="menu-item"
+        :style="{'--menu-level': menuLevel}"
+      >
+        <router-link
+          class="nav-item"
+          :to="menu.url"
+          @click="pushRouter(menu)"
+        >
+          <span>{{ menu.label }}</span>
+        </router-link>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+import {MenuBean} from '../../../interface/menuInterface'
+
+export default defineComponent({
+  name: 'AdminMenuTopChild',
+  props: {
+    menus: {
+      type: Array as PropType<MenuBean[]>,
+      default: () => {
+        return []
+      }
+    },
+    menuId: {
+      type: String,
+      default: ''
+    },
+    menuLevel: {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: ['push-router'],
+  setup (props, { emit }) {
+    const menuGroupIcon = 'Dot'
+
+    const pushRouter = (menu: MenuBean) => {
+      if (menu.id === props.menuId) {
+        return
+      }
+      emit('push-router', menu)
+    }
+
+    return {
+      menuGroupIcon,
+      pushRouter
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+  @use "@assets/scss/admin/_admin-menu-top-child.scss";
+</style>
